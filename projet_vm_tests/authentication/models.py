81082334308de1c50937e9202561ae1988e09007
moha7bn7 <moha.benkhadda@gmail.com>
2024-03-21 14:54:23 +0100

    first commit

---
+++ projet_vm_tests/authentication/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
